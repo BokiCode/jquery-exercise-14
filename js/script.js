6bba08cd98f97cc12313f0c7bd2ad2bd93160f2a
BokiCode <bojancodedev@gmail.com>
2024-02-01 17:25:50 +0100

    Everything finished

--- js/script.js
+++ js/script.js
@@ -1,25 +1,14 @@
 $(document).ready(function () {
-    const userName = "admin";
-    const password = "123";
-    let attempts = 0;
-    $("#loginButton").click(function () {
-        $(" #wrongUsername, #wrongPassword").hide();
-        const enteredUsername = $("#userName").val();
-        const enteredPassword = $("#password").val();
-        if (enteredUsername === userName && enteredPassword === password) {
-            $("body").html("Uspesno ste se ulogovali");
-            $("#password, #userName, #loginButton").hide();
-        } else {
-            attempts++;
-        }
-        if (enteredUsername !== userName) {
-            $("#wrongUsername").show();
-        }
-        if (enteredPassword !== password) {
-            $("#wrongPassword").show();
-        }
-        if (attempts === 3) {
-            $("#userName, #password, #loginButton").attr('disabled', true);
-        }
+    $("#inputWidth,#inputHeight,#inputColor,#inputBorderRadius").on("input", function () {
+        let customWidth = $("#inputWidth").val();
+        let customHeight = $("#inputHeight").val();
+        let customColor = $("#inputColor").val();
+        let customBorderRadius = $("#inputBorderRadius").val() + "%";
+        $("#element").css({
+            width: customWidth,
+            height: customHeight,
+            backgroundColor: customColor,
+            borderRadius: customBorderRadius
+        });
     });
 });
