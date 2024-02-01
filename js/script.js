8e0157cc3fdc9b902044ab66dac7922d2fcb9830
BokiCode <bojancodedev@gmail.com>
2024-02-01 16:30:00 +0100

    first part of the exercise done

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 $(document).ready(function () {
-    const userName="admin";
+    const userName = "admin";
     const password = "123";
+    let attempts = 0;
     $("#loginButton").click(function () {
+        $(" #wrongUsername, #wrongPassword").hide();
         const enteredUsername = $("#userName").val();
         const enteredPassword = $("#password").val();
-        if(enteredUsername === userName && enteredPassword === password) {
+        if (enteredUsername === userName && enteredPassword === password) {
             $("body").html("Uspesno ste se ulogovali");
             $("#password, #userName, #loginButton").hide();
         } else {
-            alert("Nalog ne postojii");
+            attempts++;
+        }
+        if (enteredUsername !== userName) {
+            $("#wrongUsername").show();
+        }
+        if (enteredPassword !== password) {
+            $("#wrongPassword").show();
+        }
+        if (attempts === 3) {
+            $("#userName, #password, #loginButton").attr('disabled', true);
         }
     });
 });
