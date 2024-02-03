ad530a35b85b2976672179d0eb79c1537f3823fd
BokiCode <bojancodedev@gmail.com>
2024-02-03 14:14:54 +0100

    removed unnecessary variables

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
 $(document).ready(function () {
     $("#inputWidth,#inputHeight,#inputColor,#inputBorderRadius").on("input", function () {
-        let customWidth = $("#inputWidth").val();
-        let customHeight = $("#inputHeight").val();
-        let customColor = $("#inputColor").val();
-        let customBorderRadius = $("#inputBorderRadius").val() + "%";
         $("#element").css({
-            width: customWidth,
-            height: customHeight,
-            backgroundColor: customColor,
-            borderRadius: customBorderRadius
+            width: $("#inputWidth").val(),
+            height: $("#inputHeight").val(),
+            backgroundColor: $("#inputColor").val(),
+            borderRadius: $("#inputBorderRadius").val() + "%"
         });
     });
 });
